248c36030090a51cd43fe4e698204b64d56ab36c
addisons <1043791113@qq.com>
2020-01-19 10:55:54 +0800

    maven-profile多环境配置

---
+++ profiles-study/src/main/java/cn/emam/profilesstudy/ProfilesStudyApplication.java
@@ -0,0 +1,13 @@
+package cn.emam.profilesstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfilesStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfilesStudyApplication.class, args);
+    }
+
+}
---
+++ profiles-study/src/test/java/cn/emam/profilesstudy/ProfilesStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.emam.profilesstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfilesStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
